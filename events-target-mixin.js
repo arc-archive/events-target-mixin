68af9c0b19b584862e157946a4afc56c9978a7b7
Pawel Psztyc <jarrodek@gmail.com>
2019-04-02 22:45:06 -0700

    Update: Updating import paths to match web spec

--- events-target-mixin.js
+++ events-target-mixin.js
@@ -11,7 +11,7 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {dedupingMixin} from '@polymer/polymer/lib/utils/mixin.js';
+import {dedupingMixin} from '../../@polymer/polymer/lib/utils/mixin.js';
 /**
  * `ArcBehaviors.EventsTargetBehavior` is a behavior mixin that allows setting
  * up event listeners on a default or set node.
